32fa35ed771c2575399a50657a7242ba0d9e0089
Jon Douglas <jodou@microsoft.com>
2019-06-19 16:19:14 -0600

    Remove Forms to Start UI

---
+++ Xamarin.Android/Finish/MonkeyFinder/MonkeyFinder/ViewModel/MonkeyDetailsViewModel.cs
@@ -0,0 +1,49 @@
+﻿using MonkeyFinder.Model;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+
+namespace MonkeyFinder.ViewModel
+{
+    public class MonkeyDetailsViewModel : BaseViewModel
+    {
+        public MonkeyDetailsViewModel()
+        {
+
+        }
+
+        public MonkeyDetailsViewModel(Monkey monkey)
+            : this()
+        {
+            Monkey = monkey;
+            Title = $"{Monkey.Name} Details";
+        }
+        Monkey monkey;
+        public Monkey Monkey
+        {
+            get => monkey;
+            set
+            {
+                if (monkey == value)
+                    return;
+
+                monkey = value;
+                OnPropertyChanged();
+            }
+        }
+
+        async Task OpenMapAsync()
+        {
+            try
+            {
+                await Map.OpenAsync(Monkey.Latitude, Monkey.Longitude);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Unable to launch maps: {ex.Message}");
+                //await Application.Current.MainPage.DisplayAlert("Error, no Maps app!", ex.Message, "OK");
+            }
+        }
+    }
+}
